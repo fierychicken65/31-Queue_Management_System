42d39b8b1e59020b4214e8ddfce0053a2cb1b66e
sashshaikh12 <sashshaik12@gmail.com>
2023-11-04 19:25:55 +0530

    changes

--- hello/home/views.py
+++ hello/home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is homepage")
+    return render(request,'index.html')
+
+def qrcode(request):
+    return render(request,"qr.html")
